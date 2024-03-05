568425596a5c4fdd8aacdc43019c18092a2f17e8
Kyle Bedell <kgbedell@gmail.com>
2024-03-05 14:16:30 -0800

    feat(books): add useBooks hook to handle consuming api

---
+++ src/hooks/useBooks.ts
@@ -0,0 +1,26 @@
+import { BOOKS_API_URL } from "@/constants/books";
+import { getBooks, createBook, deleteBook } from "@/data/api/books";
+import { Book } from "@/types";
+import useSWR, { mutate } from "swr";
+
+export default function useBooks() {
+  const { data, error, isLoading } = useSWR(BOOKS_API_URL, getBooks);
+
+  const removeBook = async (id: string) => {
+    await deleteBook(id);
+    mutate(BOOKS_API_URL);
+  };
+
+  const addBook = async (bookData: Book) => {
+    await createBook(bookData);
+    mutate(BOOKS_API_URL);
+  };
+
+  return {
+    data: data ?? [],
+    isLoading,
+    addBook,
+    removeBook,
+    error,
+  };
+}
